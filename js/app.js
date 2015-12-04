9e8047c8637e5c8a2dbe5ce25a08363315f5a2f5
Matias Cao <caomatias@gmail.com>
2015-12-04 19:55:05 -0300

    Add pomodoro cycle and tracking

--- js/app.js
+++ js/app.js
@@ -3,10 +3,19 @@ var pomodoros = 0;
 
 $(document).ready(function() {
 	$('#runner').runner({
-		startAt: 1000*60*25,
+		startAt: 5000,//1000*60*25,
 		stopAt: 0,
 		countdown: true
-	});
+	}).on('runnerFinish', function() {
+		pomodoros++;
+		console.log(pomodoros);
+		$('#runner').runner({
+			startAt: 5000,//1000*60*25,
+			stopAt: 0,
+			countdown: true,
+			autostart: true
+		});
+	})
 
 	$('#start').click(function() {
 		$('#runner').runner('start');
