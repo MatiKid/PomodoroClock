9083d4406e7e70eabb1f365a45029ec102e777c2
Matias Cao <caomatias@gmail.com>
2015-12-10 23:43:52 -0300

    Add custom timer length

--- js/app.js
+++ js/app.js
@@ -1,32 +1,37 @@
 var pomodoros = 1;
 var loop = 1;
+var pomodoroLength = 25 * 60 * 1000;
+var breakLength = 2 * 60 * 1000;
 
 
 $(document).ready(function() {
-	$('#runner').runner({
-		startAt: 10000, //1000*60*25,
-		milliseconds: false,
-		stopAt: 0,
-		countdown: true
-	}).on('runnerFinish', function(eventObject, info) {
 
-		if(loop % 2 === 0) {
-			pomodoros++;
-			loop++;
-			$('#pomodoroNum').text('Pomodoro #' + pomodoros);
+	function timer() {
+		$('#runner').runner({
+			startAt: pomodoroLength,
+			milliseconds: false,
+			stopAt: 0,
+			countdown: true
+		}).on('runnerFinish', function(eventObject, info) {
 
-			info.settings.autostart = true;
-			info.settings.startAt = 10000;
-			$('#runner').runner('start');	
-		} else if (loop % 2 !== 0) {
-			loop++;
-			$('#pomodoroNum').text('Break');
+			if(loop % 2 === 0) {
+				pomodoros++;
+				loop++;
+				$('#pomodoroNum').text('Pomodoro #' + pomodoros);
 
-			info.settings.autostart = true;
-			info.settings.startAt = 5000;
-			$('#runner').runner('start');
-		};
-	});
+				info.settings.autostart = true;
+				info.settings.startAt = pomodoroLength;
+				$('#runner').runner('start');	
+			} else if (loop % 2 !== 0) {
+				loop++;
+				$('#pomodoroNum').text('Break');
+
+				info.settings.autostart = true;
+				info.settings.startAt = breakLength;
+				$('#runner').runner('start');
+			};
+		});
+	};
 
 	$('#main-btn').click(function() {
 		$('#runner').runner('toggle');
@@ -39,4 +44,22 @@ $(document).ready(function() {
 			$(this).text('Pause');
 		}
 	});
+
+	$('#set-pomodoro-length').click(function() {
+		var pomodoroLengthInput = parseInt($('#pomodoro-length').val());
+		if(!isNaN(pomodoroLengthInput)) {
+			pomodoroLength = pomodoroLengthInput * 60 * 1000;
+			timer();
+		}
+	});
+	$('#set-break-length').click(function() {
+		var breakLengthInput = parseInt($('#break-length').val());
+		if(!isNaN(breakLengthInput)) {
+			breakLength = breakLengthInput * 60 * 1000;
+			timer();
+		}
+	});
+
+	timer();
 });
+
