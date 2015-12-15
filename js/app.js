2a923e117270df0d2caf8f14a8220ed7ba199b17
Matias Cao <caomatias@gmail.com>
2015-12-14 21:08:00 -0300

    Fix various bugs fix set pomodo/break custom time fix pause/resume button

--- js/app.js
+++ js/app.js
@@ -1,47 +1,55 @@
 var pomodoros = 1;
 var loop = 1;
-var pomodoroLength = 25 * 60 * 1000;
-var breakLength = 2 * 60 * 1000;
+var pomodoroLength = 10000//25 * 60 * 1000;
+var breakLength = 5000//2 * 60 * 1000;
 
 
 $(document).ready(function() {
 
-	function timer() {
-		$('#runner').runner({
-			startAt: pomodoroLength,
-			milliseconds: false,
-			stopAt: 0,
-			countdown: true
-		}).on('runnerFinish', function(eventObject, info) {
+	$('#runner').runner({
+		startAt: pomodoroLength,
+		milliseconds: false,
+		stopAt: 0,
+		countdown: true
+	}).on('runnerFinish', function(eventObject, info) {
 
-			if(loop % 2 === 0) {
-				pomodoros++;
-				loop++;
-				$('#pomodoroNum').text('Pomodoro #' + pomodoros);
+		if(loop % 2 === 0) {
+			pomodoros++;
+			loop++;
+			
+			$('#pomodoroNum').text('Pomodoro #' + pomodoros);
 
-				info.settings.autostart = true;
-				info.settings.startAt = pomodoroLength;
-				$('#runner').runner('start');	
-			} else if (loop % 2 !== 0) {
-				loop++;
-				$('#pomodoroNum').text('Break');
+			info.settings.startAt = pomodoroLength;
+			$('#runner').runner('start');
 
-				info.settings.autostart = true;
-				info.settings.startAt = breakLength;
-				$('#runner').runner('start');
-			};
-		});
-	};
+			// if($('#automatic-cycle').is(':checked')) {
+			// 	console.log('checked');
+			// } else {
+			// 	$('#runner').runner('start');
+			// 	console.log('NOT checked');
+			// };
+				
+		} else if (loop % 2 !== 0) {
+			loop++;
+
+			$('#pomodoroNum').text('Break');
+
+			info.settings.startAt = breakLength;
+			$('#runner').runner('start');
+		};
+	});
+
 
 	$('#main-btn').click(function() {
-		$('#runner').runner('toggle');
-
 		if($(this).text() === 'Start') {
 			$(this).text('Pause');
+			$('#runner').runner('start');
 		} else if($(this).text() === 'Pause') {
 			$(this).text('Resume');
+			$('#runner').runner('stop');
 		} else if($(this).text() === 'Resume') {
 			$(this).text('Pause');
+			$('#runner').runner('start');
 		}
 	});
 
@@ -49,17 +57,26 @@ $(document).ready(function() {
 		var pomodoroLengthInput = parseInt($('#pomodoro-length').val());
 		if(!isNaN(pomodoroLengthInput)) {
 			pomodoroLength = pomodoroLengthInput * 60 * 1000;
-			timer();
+
+			if($('#pomodoroNum').text() !== 'Break') {
+				var runnerInfo = $('#runner').runner('info');
+				runnerInfo.settings.startAt = pomodoroLength;
+				$('#runner').runner('reset');
+			}
 		}
 	});
+
 	$('#set-break-length').click(function() {
 		var breakLengthInput = parseInt($('#break-length').val());
 		if(!isNaN(breakLengthInput)) {
 			breakLength = breakLengthInput * 60 * 1000;
-			timer();
+			
+			if($('#pomodoroNum').text() === 'Break') {
+				var runnerInfo = $('#runner').runner('info');
+				runnerInfo.settings.startAt = breakLength;
+				$('#runner').runner('reset');
+			}
 		}
 	});
-
-	timer();
 });
 
